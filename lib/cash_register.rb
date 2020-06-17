e40c1fbfcd87a184cec9483eba92e9152fb6f9b1
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-16 20:22:28 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+class CashRegister
 
+    attr_accessor :total
+
+
+    def initialize(total)
+
+        @total = 0
+
+    end
+
+    def total
+
+
+    end
+
+    def add_item(item_name, price)
+
+
+    end
+
+    def apply_discount
+
+
+    end
+
+
+    def items
+
+
+    end
+
+
+    def void_last_transaction
+
+
+    end
+
+end
