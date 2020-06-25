c3fc5751abacf2cc0cf8abc180300ea14c2419d0
ndorazil <nathandorazil@live.com>
2020-06-24 21:42:40 -0600

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+class CashRegister
+    attr_accessor :total
 
+    def initialize(total=0)
+        @total = total
+
+    end
+
+    def apply_discount
+        
+    end
+
+    def add_item(item, price)
+        @price = price
+        @item = item
+        price = self.total + price
+    end
+
+    def items
+        
+    end
+end
